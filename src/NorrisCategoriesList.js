8cc82043288c1255a12b2f6c36f106e584e2c421
Freddy Brasileiro <freddybrasileiro@users.noreply.github.com>
2018-01-25 11:40:23 -0200

    calling api to get categories

--- src/NorrisCategoriesList.js
+++ src/NorrisCategoriesList.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import * as NorrisApi from './NorrisApi';
 import './GuiaNorris.css';
 
 class NorrisCategoriesList extends Component {
+  constructor(props){
+    super(props);
+
+    NorrisApi.getNorrisCategories()
+    .then(categories => console.log(categories.data));
+  }
+
   render() {
     return (
       <div className="norris-categories-list">
